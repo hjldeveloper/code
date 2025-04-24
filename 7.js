52d5c8ca8e1a21a1f16cd6fa3875bf8b2bde0936
hjldeveloper <teddycns26@gmail.com>
2025-04-24 23:33:19 +0900

    code update

--- 7.js
+++ 7.js
@@ -1,3 +1,7 @@
+// 1~n 까지의 정수로 배열 사이즈 5인 모든 경우의 수의 배열 생성
+// q[i]의 배열과 하나씩 비교한 후 일치하는 숫자가 ans[i]의 개수와 동일한 지 확인
+// 동일할 경우 count+1
+
 function solution(n, q, ans) {
   const combinations = getCombArr(
     Array.from({ length: n }, (_, i) => i + 1),
